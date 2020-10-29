a056a5047ebb6650c7d8a914e19aa85a163b8a88
Kim Sheng Yong <kimshengyong@gmail.com>
2020-10-29 18:01:30 +0800

    Create core and component folder, core folder handles character body and movement script, components folder handles character main functionalities

---
+++ Assets/Scripts/Components/CharacterRun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRun : CharacterComponents
+{
+  // Start is called before the first frame update
+  [SerializeField] private float runSpeed = 10f;
+  protected override void HandleInput()
+  {
+    if (Input.GetKey(KeyCode.LeftShift))
+    {
+      Run();
+    }
+    if (Input.GetKeyUp(KeyCode.LeftShift)) // not pressing
+    {
+      StopRun();
+    }
+  }
+
+  private void Run()
+  {
+    characterMovement.MoveSpeed = runSpeed;
+  }
+  private void StopRun()
+  {
+    characterMovement.ResetSpeed(); //then reset speed back to walk speed
+  }
+
+}

cb4b89f213524786b5ee6d858b76f00386d9da1d
Kenneth Tan <kttan1999@gmail.com>
2020-11-23 16:20:07 +0800

    Add new font, flashing red animation, damage particles and sound effect

---
+++ Assets/Scripts/Ken/KenFlash.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class KenFlash : Flashable
+{
+	private ParticleSystem blood;
+	private AudioSource hurt;
+
+	protected override void Start()
+	{
+		base.Start();
+		blood = GetComponentInChildren<ParticleSystem>();
+		hurt = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flash();
+			blood.Play();
+			hurt.Play();
+        }
+    }
+}

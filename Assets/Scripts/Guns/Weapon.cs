1ced1f84438256074744c498807ce6fa1c96c60e
Tuan Hong <tuanhong.0820@gmail.com>
2020-11-28 18:10:40 +0800

    character able to hold weapon & weapon can rotate

--- Assets/Scripts/Guns/Weapon.cs
+++ Assets/Scripts/Guns/Weapon.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * This class is to control the movement of weapon
+ * However, the rotation of the weapon is controlled by WeaponAim
+ */
+
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem muzzlePS;
+
+    // Reference of the Character that controls this Weapon
+    public Character WeaponOwner { get; set; }
+
+    private Animator animator;
+    private readonly int weaponUseParameter = Animator.StringToHash("WeaponUse");
+
+
+    protected virtual void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+        RotateWeapon();
+    }
+
+    // Reference the owner of this Weapon
+    public void SetOwner(Character owner)
+    {
+        WeaponOwner = owner;
+    }
+
+    protected virtual void RotateWeapon()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
     }
 }

981e33f0e8107762a9483ca161de94adca441e2c
Kenneth Tan <kttan1999@gmail.com>
2020-12-04 11:55:22 +0800

    Documentation changes

--- Assets/Scripts/Common/ForwardOrientable.cs
+++ Assets/Scripts/Common/ForwardOrientable.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Makes it such that the sprite will always face the direction that it is going.
+/// Makes it such that the sprite will always face the direction that it is going.<br/>
 /// 
 /// For this to work properly, the GameObject's sprite must be facing to the right and does not have
 /// the SpriteFlippable2D component.

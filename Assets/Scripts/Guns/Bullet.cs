5b2e1e69691fbff627c2b8d42a84b8d87e4294c2
Kim Sheng Yong <kimshengyong@gmail.com>
2020-12-13 22:31:33 +0800

    add 5 zombies with ability to receive attack, problem with enemy projectile on player

---
+++ Assets/Scripts/Guns/Bullet.cs
@@ -0,0 +1,17 @@
+﻿// using UnityEngine;
+
+// public class Bullet : Movable2D
+// {
+// 	public GameObject hitEffect;
+// 	public GameObject bullet;
+
+// 	void OnCollisionEnter2D(Collision2D collision)
+// 	{
+
+// 	}
+	
+// 	void OnBecameInvisible()
+// 	{
+// 		Destroy(bullet);
+// 	}
+// }

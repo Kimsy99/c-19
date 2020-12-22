3ed7807b4d6230e73681e75015b6195b6789640e
Tuan Hong <tuanhong.0820@gmail.com>
2020-12-23 02:08:30 +0800

    add music

---
+++ Assets/Scripts/Controller/OnTriggerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerController : MonoBehaviour
+{
+    [Header("Game Object to be set active on trigger")]
+    [SerializeField] GameObject GameObject;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameObject.SetActive(true);
+        }    
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameObject.SetActive(false);
+        }
+    }
+}

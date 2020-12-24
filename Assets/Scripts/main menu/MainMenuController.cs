eac6729f528b0aefa60fe0f68f90a7c703d97659
Mister_Bander <kttan1999@gmail.com>
2020-12-24 11:35:46 +0800

    Finish main menu

---
+++ Assets/Scripts/main menu/MainMenuController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+ 
+public class MainMenuController : MonoBehaviour
+{
+	public GameObject mainMenu;
+	public GameObject optionsMenu;
+
+	public void Play()
+	{
+		mainMenu.SetActive(false);
+		optionsMenu.SetActive(true);
+	}
+
+	public void Back()
+	{
+		mainMenu.SetActive(true);
+		optionsMenu.SetActive(false);
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+
+	public void Level1()
+	{
+	   
+	}
+
+	public void Level2()
+	{
+
+	}
+
+	public void Level3()
+	{
+
+	}
+
+	public void Level4()
+	{
+
+	}
+
+	public void Level5()
+	{
+
+	}
+}
